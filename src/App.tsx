fb0ed9ce8a4cb87b6b29510985eab561037ddad9
sushil <sushil124maurya@gmail.com>
2024-03-08 12:52:47 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { Button } from 'primereact/button';                             
+function App() {
+
+  return (
+    <>
+      <Button >Sushil</Button>
+    </>
+  )
+}
+
+export default App
